2ea7557b54b5a099e20d97ce314725d7e5394065
Daniel Ash <d_ash12345@hotmail.co.uk>
2021-01-16 01:49:11 +0000

    Fixed issues with git directory.

--- test.rb
+++ test.rb
@@ -1,13 +1,6 @@
 COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8]].freeze
 
-test = ['X', 'O', 'X', 'O', 5, 6, 'X', 'O', 'X']
+test = gets.chomp
 
-result = COMBINATIONS.any? do |i|
-  i.all? { |element| test[element] == 'X' }
-end
-p result
-# use a combination of .any .all
 
-test_two = ['X', 1, 2]
-
-(test_two.any? { |element| element == 'X' })
+if test != 'X' || test != 'O' then p 'invalid' end
