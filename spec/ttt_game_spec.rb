4106c8d74ab777c1920ba963f9100c4afd56776d
SaltySaff <danielalexander.ash@gmail.com>
2021-12-03 17:20:33 +0000

    Added tests

--- spec/ttt_game_spec.rb
+++ spec/ttt_game_spec.rb
@@ -6,12 +6,39 @@ require_relative '../lib/ttt_player'
 
 describe Game do
   subject(:example_game) { described_class.new }
+  let(:game_board) { instance_double(Board) }
+  let(:example_player) { instance_double(Player, name: 'Example Player', choice: 'X') }
 
   describe '#validate_input' do
     # looping script method
-  end
+    # will stop when board.check_valid?(number) returns true
+    context 'when user input is incorrect once, then correct' do
+      before do
+        allow(example_game).to receive(:clear_screen)
+        allow(game_board).to receive(:display_board)
+      end
 
-  describe '#play_again?' do
-    # looping script method
+      it 'completes loop and display error message once' do
+        valid_input = 4
+        error_message = 'Invalid entry, enter a number from 1-9.'
+        expect(example_game).not_to receive(:puts).with(error_message)
+        example_game.validate_input(valid_input)
+      end
+    end
+
+    context 'when user input is incorrect once, then correct' do
+      before do
+        allow(example_game).to receive(:clear_screen)
+        allow(game_board).to receive(:display_board)
+        allow(example_game).to receive(:gets).and_return('4')
+      end
+
+      it 'completes loop and display error message once' do
+        input = 'r'
+        error_message = 'Invalid entry, enter a number from 1-9.'
+        expect(example_game).to receive(:puts).with(error_message).once
+        example_game.validate_input(input)
+      end
+    end
   end
 end
